2a5a830a387622b7d311818cd4cc284872030cf8
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-03-15 05:52:19 -0700

    printing putchar with function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* main - Print putchar in the main
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	_putchar('_putchar\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+/**
+* main - Function collectors
+*
+* Return: The return values depends on the parameter
+*/
+
+int _putchar(char character[20]);
