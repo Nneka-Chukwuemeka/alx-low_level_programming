46edc7fe835c7527f96e672535ce9856b8aa3a7e
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-03-15 08:58:00 -0700

    last digit of the number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_last_digit - printing the last number
+* @a: it is the parameter of the function
+*
+* Return: Always return 1 (Success)
+*/
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
