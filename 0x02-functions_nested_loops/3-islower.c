e7c9e322390e8ebcd350dff5778bc79da4833c2d
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-03-15 07:51:28 -0700

    its lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _islower - print the ones that are lower case
+* @c: parameter of _islower
+* Return: Always 1 (Success)
+*/
+int _islower(int c)
+{
+	int result;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		c = result;
+	}
+	return (result);
+}
