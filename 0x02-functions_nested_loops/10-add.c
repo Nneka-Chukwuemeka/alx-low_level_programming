26a017fbae161cd807be84d7bcd486baa708dd95
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-03-15 15:50:06 -0700

    Addition of numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - add two intergers
+* @a: one parameter
+* @b: second parameter
+* Return: the answer always 1 at success
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
