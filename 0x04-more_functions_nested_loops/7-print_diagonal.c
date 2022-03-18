ba40a9d895a1f3729e1c8dacd27bbbb8447e138f
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-03-18 07:46:46 -0700

    diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal line
+ * @n: include the parameter
+*/
+void print_diagonal(int n)
+{
+	int count, space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (count = 0; count < n; count ++)
+		{
+			space = n;
+			while (space > 0)
+			{
+				_putchar(' ');
+				space--;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
