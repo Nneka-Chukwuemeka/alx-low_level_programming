f8db797e464a58393a1f1ef8332f9ec7ef0ea437
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-03-21 15:33:30 -0700

    swap a for b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** swap_int - To swap the value of a and b
+ * @a: interger needed
+ * @b: interger to be included
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b);
+{
+	int c = *a;
+	*a = *b;
+	*b = *c;
+}
