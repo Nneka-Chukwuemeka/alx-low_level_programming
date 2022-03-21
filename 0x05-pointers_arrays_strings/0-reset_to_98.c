dd006027beab5bea79787c1b7aefd2505f6499c0
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-03-21 09:30:31 -0700

    updating pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#inculde "main.h"
+
+/**
+ * reset_to_98 - A pointer that updates to 98
+ * @*n: An interger that must be included
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Working with array and pointers
