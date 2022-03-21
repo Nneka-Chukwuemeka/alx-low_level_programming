b91e05dd000b41ac5bfa8e65351a063ca5075113
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-03-21 16:35:06 -0700

    printing strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints the string 
+ * *str: interger to be included
+ *
+ * Return: Nothing to return
+ */
+
+void _puts(char *str)
+{
+	int j;
+
+	for (j = 0; str[j]; j++)
+	{
+		_putchar(str[j]);
+	}
+}
