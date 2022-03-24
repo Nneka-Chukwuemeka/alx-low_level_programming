f1583c69e113a658904856c1849effd87f6fac9d
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-03-24 09:44:42 -0700

    copying strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - A function that copys one string into another
+ * with a condition of n
+ * @dest: The destination parameter
+ * @src: The source parameter
+ * @n: The string length
+ *
+ * Return: Dest will be returned
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	a = 0;
+	while (a < n)
+	{
+		dest[a] = '\0';
+	}
+	return (dest);
+}
