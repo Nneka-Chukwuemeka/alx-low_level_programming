516bccffd5e6927bf3e03ed3f44e6cdc4e89db15
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-03-28 09:39:42 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - A function that prints a particular string or character
+ * until n bytes is reached
+ * @s: The string to be checked
+ * @b: repeated parameter to be printed
+ * @n: the byte size
+ *
+ * Return: dest should be returned
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a;
+
+	for (a = 0; a <= n; a++)
+	{
+		s[a] = b;
+	}
+	return (dest);
+}
