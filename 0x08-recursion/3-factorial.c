a153d66e21ecc4c17abe4d480c4e2b9342fbe2fb
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-03-30 15:07:27 -0700

    task 3 and 4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - Write a function that prints the factorial of a number
+ * @n: The parameter to be included
+ *
+ * Return: return the value of n
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n x factorial(n - 1));	}
+}
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - A function that prints the x power y
+ * @x: It is the parameter to find its power
+ * @y: Parameter for the power of x
+ *
+ * Return: x raised to power y if > than 0;
+ */
+
+_pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * _pow_recursion(x, y - 1));
+	}
+}
