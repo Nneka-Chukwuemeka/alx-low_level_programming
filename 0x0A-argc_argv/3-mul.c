ebd6e2fd3b6638b1cb5e23adc7a8b91d9007b838
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-04-01 14:23:22 -0700

    function that multiplys two nums

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - A function that mutliplys two numbers only
+ * @argc: Parameter count
+ * @argv: argument storage
+ *
+ * Return: 0 in success and 1 in error
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1] * atoi(agrv[2]));
+	}
+	return (0);
+}
