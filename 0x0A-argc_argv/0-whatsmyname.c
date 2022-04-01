f5c4f990a9a64daec59359fdeed6d42d20797c92
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-04-01 08:24:46 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints the argument the filename
+ * @argc: An argument that count number of argument
+ * @argv: A pointer of array
+ * Return: Success is zero
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf(" %s \n", argv[0]);
+	return (0);
+}	
