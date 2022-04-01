fff9b049451545ad39d152ca9861f4232fb59c3b
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-04-01 10:17:45 -0700

    static lib

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,20 +1,11 @@
-#include "main.h"
+#include <unistd.h>
 /**
 * main - Print putchar in the main
 *
 * Return: Always 0 (Success)
 */
-int main(void)
+int _putchar(char c)
 {
-	int arry[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int arryLength = sizeof(arry) / sizeof(int);
-	int i;
 
-	for (i = 0; i < arryLength; i++)
-	{
-		_putchar(arry[i]);
-	}
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
