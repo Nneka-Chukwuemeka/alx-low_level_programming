aaeb715a00253ea7d0d27edaa6e6de5551bdcaec
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-04-01 09:41:37 -0700

    arguments that counts

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A function that counts number of arguments
+ * @argc: A parameter that returns number of arguments
+ * @argv: An undefined parametrs
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
