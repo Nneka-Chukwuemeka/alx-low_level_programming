b60f12c5949f9d9e6a22ea9f9546e1feff4c6712
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-04-05 09:54:37 -0700

    create a function with a char that check the size of the byte

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - A functions that creates an array
+ * and initialize it with a character
+ * @size: check the size of the array
+ * @c: character
+ *
+ * Return: 0 if error
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int a = 0;
+	char *p;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	p = (char *) malloc(sizeof(char) * size);
+
+	if (p == NULL)
+		return (0);
+
+	while (i < size)
+	{
+		*(p + a) = c;
+		a++;
+	}
+	*(p + i) = '\0';
+	return (p);
+}
