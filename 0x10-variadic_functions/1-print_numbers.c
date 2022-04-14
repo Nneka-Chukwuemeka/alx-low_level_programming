7320446d9da9336078789c1396a8f0113bf2a9eb
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-04-14 09:42:45 -0700

    Prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - A function that prints numbers with a new line
+ * @separator: A string that seperates the numbers
+ * @n: The number of parameter to print
+ *
+ * Return: the value if true and exit if false
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list(list);
+	unsigned int a;
+
+	va_start(list, n);
+	if (n != 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			if (separator != NULL)
+			{
+				printf("%d%s", va_arg(list, int), separator);
+			}
+			else
+			{
+				printf("%d", va_arg(list, int);
+			}
+			va_end(list);
+		}
+	}
+	printf("\n");
+}
